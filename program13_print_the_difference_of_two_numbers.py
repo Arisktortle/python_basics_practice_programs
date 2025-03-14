4dca6fe33407e61c1f4c7f33560db793b464834f
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 07:04:14 +0800

    calculate the difference and print the result

--- program13_print_the_difference_of_two_numbers.py
+++ program13_print_the_difference_of_two_numbers.py
@@ -3,3 +3,5 @@ num1 = int(input("Please input the first number: "))
 num2 = int(input("Please input the second number: "))
 
 #computes the difference and print
+difference = num1 - num2
+print("The difference calculated is:", difference)
