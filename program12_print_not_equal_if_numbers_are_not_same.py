11d4d3111158b1acffbc5a4b99efaa4b7c14d173
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 02:00:49 +0800

    checks if numbers are equal or not and prints not equal

--- program12_print_not_equal_if_numbers_are_not_same.py
+++ program12_print_not_equal_if_numbers_are_not_same.py
@@ -3,3 +3,7 @@ num1 = int(input("Please input the first number: "))
 num2 = int(input("Please input the second number: "))
 
 #check if two numbers are not same and print "NOT EQUAL"
+if num1 != num2:
+    print(num1, "and", num2, "are NOT EQUAL")
+else:
+    print(num1, "and", num2, "are EQUAL")
