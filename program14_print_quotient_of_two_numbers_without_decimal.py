4494c61c957f48688f609d482dc186cc94a54682
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 07:16:27 +0800

    fixed input two numbers description

--- program14_print_quotient_of_two_numbers_without_decimal.py
+++ program14_print_quotient_of_two_numbers_without_decimal.py
@@ -1,6 +1,6 @@
 #input two numbers from user
-num1 = int(input("Please input the minuend: "))
-num2 = int(input("Please input the subtrahend: "))
+num1 = int(input("Please input the number to calculate the quotient: "))
+num2 = int(input("Divide in what number: "))
 
 #calculate the quotient of num1 and num2
 #prints the quotient without decimal point
