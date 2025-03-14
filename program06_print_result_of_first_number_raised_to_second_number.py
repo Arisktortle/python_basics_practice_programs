afd4193d8faf048a7bf6acd40faa1fc78eeea223
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 00:18:38 +0800

    raises the first number into second and prints the result

--- program06_print_result_of_first_number_raised_to_second_number.py
+++ program06_print_result_of_first_number_raised_to_second_number.py
@@ -4,3 +4,5 @@ num1 = int(input("Please input the first number: "))
 num2 = int(input("Please input the second number: "))
 
 #raise the first number to second and print result
+result = num1 ** num2 
+print("The result is:", result)
