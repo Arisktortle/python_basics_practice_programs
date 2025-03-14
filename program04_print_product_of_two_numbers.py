7bc099d266b90fe64969b92a369324f7c93efdcc
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-14 23:57:28 +0800

    get product and print

--- program04_print_product_of_two_numbers.py
+++ program04_print_product_of_two_numbers.py
@@ -3,3 +3,5 @@ num1 = int(input("Please input the first number: "))
 num2 = int(input("Please input the second number: "))
 
 #get the product and print
+product = num1 + num2
+print("The sum of the two numbers is:", product)
