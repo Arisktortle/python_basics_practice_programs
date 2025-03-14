1c477a49f48041c323191dcd77bc43c73adbeb30
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 01:51:53 +0800

    fixing

---
+++ program11_print_smaller_number_between_two_numbers.py
@@ -0,0 +1,9 @@
+#input two numbers
+num1 = int(input("Please input the first number: "))
+num2 = int(input("Please input the second number: "))
+
+#checks the smaller number and print it
+if num1 < num2:
+    print(num1, "is the smaller number.")
+else:
+    print(num2, "is the smaller number.")
