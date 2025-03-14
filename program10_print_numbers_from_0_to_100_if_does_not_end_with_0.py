2f0103b5e068b44736fe2ec85bbc830da07f1258
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 01:28:51 +0800

    check if the number ends with 0

--- program10_print_numbers_from_0_to_100_if_does_not_end_with_0.py
+++ program10_print_numbers_from_0_to_100_if_does_not_end_with_0.py
@@ -1,4 +1,4 @@
 #create for loop starting from 0 to 100
 for number in range(101):
-#check if the number ends with 0
+    if number % 10 != 0: #check if the number ends with 0
 #prints the number if does not end with 0
