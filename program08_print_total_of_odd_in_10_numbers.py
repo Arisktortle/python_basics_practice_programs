4386d18650cc76108b20da3e575896cc22f07d46
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 00:52:29 +0800

    changed num input name

--- program08_print_total_of_odd_in_10_numbers.py
+++ program08_print_total_of_odd_in_10_numbers.py
@@ -1,7 +1,7 @@
 #input 10 numbers
 odd = 0
 for i in range(10):
-    odd += int(input(f"Please enter number {i+1}: "))
+    number += int(input(f"Please enter number {i+1}: "))
     
 #check if odd number
 #print total of odd counted
