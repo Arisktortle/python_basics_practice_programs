efbd15f58852d7379d779719f7af10f5566dbc35
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 00:55:47 +0800

    odd checker and counts if odd

--- program08_print_total_of_odd_in_10_numbers.py
+++ program08_print_total_of_odd_in_10_numbers.py
@@ -3,5 +3,8 @@ odd = 0
 for i in range(10):
     number += int(input(f"Please enter number {i+1}: "))
     
-#check if odd number
+#check if odd number and count the number of odds
+if number % 2 != 0:
+    odd += 1
+    
 #print total of odd counted
