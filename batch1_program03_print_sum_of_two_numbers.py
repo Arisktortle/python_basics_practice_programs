98eea0f81ecf7d58c4e58b570590c05f43810c5a
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 01:41:09 +0800

    changed file name

---
+++ batch1_program03_print_sum_of_two_numbers.py
@@ -0,0 +1,8 @@
+#input two numbers
+print("Welcoem to the sum computer.")
+num1 = int(input("Please input the first number: "))
+num2 = int(input("Please input the second number: "))
+
+#get the sum and print
+sum = num1 + num2
+print("The sum of the two numbers is:", sum)
