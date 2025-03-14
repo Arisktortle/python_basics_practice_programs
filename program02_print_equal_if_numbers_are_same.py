82f3b9bfbfbcfbe2423f607b953bc5e98183af55
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-14 23:37:22 +0800

    print equal if numbers are same

--- program02_print_equal_if_numbers_are_same.py
+++ program02_print_equal_if_numbers_are_same.py
@@ -3,3 +3,8 @@ num1 = int(input("Please input the first number: "))
 num2 = int(input("Please input the second number: "))
 
 #print "equal" if same numbers
+
+if num1 == num2:
+    print("Both numbers are EQUAL.")
+else:
+    print("Both numbers are NOT EQUAL.")
