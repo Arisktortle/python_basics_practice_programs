b4fb17f7f29b92f96bf95c7f44c141ef39a3b092
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 01:04:57 +0800

    fixed the sign of numbers and fixed the indention for if statement

--- program08_print_total_of_odd_in_10_numbers.py
+++ program08_print_total_of_odd_in_10_numbers.py
@@ -1,10 +1,10 @@
 #input 10 numbers
 odd = 0
 for i in range(10):
-    number += int(input(f"Please enter number {i+1}: "))
+    number = int(input(f"Please enter number {i+1}: "))
     
 #check if odd number and count the number of odds
-if number % 2 != 0:
-    odd += 1
+    if number % 2 != 0:
+       odd += 1
     
 #print total of odd counted
