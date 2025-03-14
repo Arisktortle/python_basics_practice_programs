bdf1e81cd12fca0289d7b69d2a265c9355c17597
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 07:19:12 +0800

    assign the value of quotient and calculates num1 and num2

--- program14_print_quotient_of_two_numbers_without_decimal.py
+++ program14_print_quotient_of_two_numbers_without_decimal.py
@@ -3,4 +3,6 @@ num1 = int(input("Please input the number to calculate the quotient: "))
 num2 = int(input("Divide in what number: "))
 
 #calculate the quotient of num1 and num2
+quotient = num1 / num2
+
 #prints the quotient without decimal point
