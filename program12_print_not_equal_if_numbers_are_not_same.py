7857693bbaa9c322091f4b09b9848d11fa40a588
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 01:56:22 +0800

    input two numbers

---
+++ program12_print_not_equal_if_numbers_are_not_same.py
@@ -0,0 +1,5 @@
+#input two numbers
+num1 = int(input("Please input the first number: "))
+num2 = int(input("Please input the second number: "))
+
+#check if two numbers are not same and print "NOT EQUAL"
