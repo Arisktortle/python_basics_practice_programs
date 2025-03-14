d4a287fa205f590d188d823938d9140bd6b15613
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 00:13:51 +0800

    added description and input two numbers

---
+++ program06_print_result_of_first_number_raised_to_second_number.py
@@ -0,0 +1,6 @@
+#input two numbers
+print("Welcome to first number raiser program.")
+num1 = int(input("Please input the first number: "))
+num2 = int(input("Please input the second number: "))
+
+#raise the first number to second and print result
