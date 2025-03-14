90e8cdaf7f21e987facf56d365a8360660892bb3
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 01:35:14 +0800

    check the smaller number and print

--- batch2_program01_print_smaller_number_between_two_numbers.py
+++ batch2_program01_print_smaller_number_between_two_numbers.py
@@ -3,3 +3,7 @@ num1 = int(input("Please input the first number: "))
 num2 = int(input("Please input the second number: "))
 
 #checks the smaller number and print it
+if num1 < num2:
+    print(num1, "is the smaller number.")
+else:
+    print(num2, "is the smaller number.")
