a3efca79db335869d9bf0a6ac35c80f22e2d5a7a
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 00:04:49 +0800

    fixed variable type, gets the quotient and print result

--- program05_print_quotient_of_two_numbers.py
+++ program05_print_quotient_of_two_numbers.py
@@ -1,5 +1,7 @@
 #input two numbers
-num1 = int(input("Enter the number to divide: "))
-num2 = int(input("Divide in what number: "))
+num1 = float(input("Enter the number to divide: "))
+num2 = float(input("Divide in what number: "))
 
 #get the quotient and print with decimal point
+quotient = num1 / num2 
+print("The quotient is:", quotient)
