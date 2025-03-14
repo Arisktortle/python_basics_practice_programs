c0c57bca56e89ad85941a890916cd4c9be6ba7d9
Synch Coding <dhdbbdhd161@gmail.com>
2025-03-15 01:44:04 +0800

    Delete batch1_program05_print_quotient_of_two_numbers.py

--- batch1_program05_print_quotient_of_two_numbers.py
+++
@@ -1,8 +0,0 @@
-#input two numbers
-print("Welcome to the quotient computer program.")
-num1 = float(input("Enter the number to divide: "))
-num2 = float(input("Divide in what number: "))
-
-#get the quotient and print with decimal point
-quotient = num1 / num2 
-print("The quotient is:", quotient)
