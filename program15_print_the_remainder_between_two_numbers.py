a2ed12f526a7c6cc805994532dcddd2f1f74f2dc
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 07:46:07 +0800

    calculates the remainder of the two numbers and print

--- program15_print_the_remainder_between_two_numbers.py
+++ program15_print_the_remainder_between_two_numbers.py
@@ -1,5 +1,8 @@
 #input two numbers
-num1 = int(input("Please enter the first number to divide:"))
-num2 = int(input("Enter the divisor for the first number:"))
+num1 = int(input("Please enter the first number to divide: "))
+num2 = int(input("Enter the divisor for the first number: "))
 
 #calculate and print the remainder of the two numbers divided
+remainder = num1 % num2
+print("The remainder is:", remainder)
+
