b9f42a3c70baae0a940fa8bc3892a7cfe0006c8d
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 00:38:44 +0800

    intialized the sum variable and fixed the spacing in inputting numbers

--- program07_print_sum_of_10_numbers.py
+++ program07_print_sum_of_10_numbers.py
@@ -1,5 +1,6 @@
 #input 10 numbers from user
+sum = 0
 for i in range(10):
-    sum += int(input(f"Please enter 10 numbers{i+1}:"))
+    sum += int(input(f"Please enter number {i+1}: "))
     
 #print sum of the numbers
