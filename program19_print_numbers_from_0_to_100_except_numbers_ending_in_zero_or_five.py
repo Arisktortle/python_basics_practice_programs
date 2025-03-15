a6a6bc42c470cab86ea59d7774e2a7fc9edb1f28
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 08:30:35 +0800

    created a loop that checks and print if the number does not end in 0 or 5

--- program19_print_numbers_from_0_to_100_except_numbers_ending_in_zero_or_five.py
+++ program19_print_numbers_from_0_to_100_except_numbers_ending_in_zero_or_five.py
@@ -1,3 +1,4 @@
 #create a loop that prints 0 to 100 numbers
-#checks if the number ends with 0 or 5
-#prints the number not ending in 0 or 5
+for num in range(101):
+    if num % 10 != 0 and num %10 !=5: #checks if the number ends with 0 or 5
+        print(num) #prints the number not ending in 0 or 5
