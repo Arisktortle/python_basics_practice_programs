4fdbcc054d32a9d9a00c07e0b311e6dc53bb3160
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:19:16 +0800

    input 10 numbers then check for value error

--- program27_print_the_number_with_most_duplicates.py
+++ program27_print_the_number_with_most_duplicates.py
@@ -2,7 +2,19 @@
 number_count = {}
 
 #input 10 numbers
+while True:
+    user_input = input("Please enter a number: ")
+
+    #check if valid
+    if user_input.isdigit() or (user_input.startswith('-') and user_input[1:].isdigit()):
+        number = int(user_input)
+        
 #count for the number times the number is inputted
+
 #if an input is invalid exit program
+    else:
+        print("Invalid input! Exiting...")
+        break
+    
 #check the number with the most duplicates
 #print that number
