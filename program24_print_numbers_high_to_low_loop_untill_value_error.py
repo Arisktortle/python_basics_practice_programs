ad8f1794a2e09daebb68faa16b94a23061c75369
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 09:28:38 +0800

    loop that prompts to enter numbers and check if value error

--- program24_print_numbers_high_to_low_loop_untill_value_error.py
+++ program24_print_numbers_high_to_low_loop_untill_value_error.py
@@ -2,7 +2,15 @@
 numbers = []
 
 #loop that ask to input numbers
+while True:
+    try:
+        num = int(input("Enter a number (or type something non-numeric to exit): "))
 #adds the inputted number into the list
+
 #check for value error, terminate if yes
+    except ValueError:
+        print("Invalid input. Exiting the program.")
+        break
+    
 #sort the number inputted in decreasing
 #print the inputted numbers from the list
