519f2593a3fb462fa1cf2429a174fd337ae1f603
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:53:43 +0800

    input numbers until value error, store valid numbers into the list

--- program25_print_numbers_low_to_high_loop_untill_value_error.py
+++ program25_print_numbers_low_to_high_loop_untill_value_error.py
@@ -1,8 +1,18 @@
 #create a list for number inputs
 numbers = []
 
-#input numbers and loop untill value error
-#check if valid input, then add to the list
-#if invalid input break the loop
+#input numbers and loop until value error
+while True:
+    user_input = input("Please enter a number: ")
+
+    #check if valid input, then add to the list
+    if user_input.isdigit() or (user_input.startswith('-') and user_input[1:].isdigit()): 
+        number = int(user_input)
+        numbers.append(number) 
+    else:
+        #if invalid input break the loop
+        print("Invalid input! Exiting...")
+        break
+    
 #arrange the list in ascending order
 #print the list from lowest to highest
