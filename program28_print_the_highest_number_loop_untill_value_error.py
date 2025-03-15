bfb16c44d0c344c8bcf4390676de6b7854ba144d
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:27:56 +0800

    pseudocode

---
+++ program28_print_the_highest_number_loop_untill_value_error.py
@@ -0,0 +1,6 @@
+#create a variable for highest number
+#create loop that input numbers 
+#checks if the input is valid
+#intialize in the count and store in the variable if highest number
+#terminate the program if invalid input
+#print the highest number input
