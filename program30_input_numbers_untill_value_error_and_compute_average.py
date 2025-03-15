d3c3161bdfffe271d4d99ef3180af37292b0039f
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:44:31 +0800

    intialize value for sum and total numbers

--- program30_input_numbers_untill_value_error_and_compute_average.py
+++ program30_input_numbers_untill_value_error_and_compute_average.py
@@ -1,4 +1,7 @@
 #create value for sum and total count of numbers
+sum = 0
+numbers_count = 0
+
 #create loop that asks user for numbers untill value error
 #checks if the input is valid
 #if the input is valid break the loop
