4beffc987090b5afdb985961d6d93864cc28ddc0
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 09:58:39 +0800

    added loop for entering numbers, then check if value is lowest then store in the variable intialized then check for invalid input

--- program24_print_lowest_number_loop_untill_value_error.py
+++ program24_print_lowest_number_loop_untill_value_error.py
@@ -2,6 +2,19 @@
 lowest_number = None
 
 #create loop that input numbers
-#check for invalid input
-#if invalid input break loop
+while True:
+    user_input = input("Please enter a number: ")
+
+    try:
+        number = int(user_input)
+        
+        #if valid input, and lowest number, set the value for lowest_number
+        if lowest_number is None or number < lowest_number:
+            lowest_number = number
+
+    #check for invalid input
+    except ValueError:
+        print("Invalid input! Exiting...") #if invalid input break loop
+        break
+    
 #print the numbers entered after loop terminate
