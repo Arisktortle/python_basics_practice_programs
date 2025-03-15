156498d0a47ce8cbb52ae333a3af17648630a97b
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 09:07:53 +0800

    check the first entry and prints only the first

--- program22_display_numbers_those_with_duplicate_display_only_first_entry.py
+++ program22_display_numbers_those_with_duplicate_display_only_first_entry.py
@@ -2,3 +2,6 @@
 nums = [int(input(f"Enter number {i+1}: ")) for i in range(10)]
 
 #check for duplicates, print only the first entry
+for i in range(10):
+    if nums.count(nums[i]) == 1 or nums.index(nums[i]) == i: 
+        print(nums[i]) #print numbers, those with dupes only first entry
