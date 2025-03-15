59dfb9da1c2933b23e49a31bcb6e94c008f0068e
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:29:17 +0800

    storage for highest number input

--- program28_print_the_highest_number_loop_untill_value_error.py
+++ program28_print_the_highest_number_loop_untill_value_error.py
@@ -1,4 +1,6 @@
 #create a variable for highest number
+highest_number = None
+
 #create loop that input numbers 
 #checks if the input is valid
 #intialize in the count and store in the variable if highest number
