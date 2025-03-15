7560c2564f343ce3fae86c863000aec9612f137d
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:39:32 +0800

    input numbers and checks for value error, if invalid input terminates the loop

--- program29_print_numbers_highest_to_lowest_loop_untill_value_error.py
+++ program29_print_numbers_highest_to_lowest_loop_untill_value_error.py
@@ -2,6 +2,18 @@
 numbers = []
 
 #create a loop that input numbers
-#check the input for value error
+while True:
+    user_input = input("Please enter a number: ")
+    
+    
+    if user_input.isdigit() or (user_input.startswith('-') and user_input[1:].isdigit()): #check the input for value error
+        number = int(user_input)
+        numbers.append(number)  #places the input into the numbers list
+        
+    else:
+        #if value error terminate the loop
+        print("Invalid input! Exiting...")
+        break
+    
 #arrange the inputs highest to lowest
 #print the inputs in descending order
