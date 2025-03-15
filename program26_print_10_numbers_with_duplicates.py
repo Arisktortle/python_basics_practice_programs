f4680065c7c6ae36714dca9417884196bcb7f887
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:06:00 +0800

    create loop to input 10 numbers and put inside the list, intialize a dictionary for number counts

--- program26_print_10_numbers_with_duplicates.py
+++ program26_print_10_numbers_with_duplicates.py
@@ -2,5 +2,12 @@
 numbers = []
 
 #input 10 numbers with loop
+for i in range(10):
+    number = int(input(f"Please enter number {i+1}: "))
+    numbers.append(number)
+    
+#create dictionary to check number count for checking dupes
+number_count = {}
+
 #check for duplicates of input numbers
 #print numbers with dupes
