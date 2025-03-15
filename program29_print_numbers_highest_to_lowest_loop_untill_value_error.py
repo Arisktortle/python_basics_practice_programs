a27e558e582e66c4233b85f7808a4ff4dcff2fcf
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:36:46 +0800

    pseudocode

---
+++ program29_print_numbers_highest_to_lowest_loop_untill_value_error.py
@@ -0,0 +1,5 @@
+#create a list to store number inputs
+#create a loop that input numbers
+#check the input for value error
+#arrange the inputs highest to lowest
+#print the inputs in descending order
