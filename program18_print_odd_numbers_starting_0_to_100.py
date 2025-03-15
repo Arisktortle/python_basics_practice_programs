24a2160da58c3827f805971dc40d268530725677
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 08:23:45 +0800

    for loop starting from 1 to 100 and increments by 2's to print odd numbers

--- program18_print_odd_numbers_starting_0_to_100.py
+++ program18_print_odd_numbers_starting_0_to_100.py
@@ -1,3 +1,3 @@
-#opt the starting number
 #create a loop up to 0 to 100 the value starts with 1 and increments 
-#print the odd numbers
+for num in range(1, 101, 2):  #create a loop up to 0 to 100 the value starts with 1 and add by 2's 
+    print(num) #print the odd numbers
