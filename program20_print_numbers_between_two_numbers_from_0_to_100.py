343db7ade916b7feb78c981405d6569b585da49c
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 08:43:38 +0800

    loop ranges 0 to 100, checks and print numbers in between the two numbers

--- program20_print_numbers_between_two_numbers_from_0_to_100.py
+++ program20_print_numbers_between_two_numbers_from_0_to_100.py
@@ -3,3 +3,5 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 #loop that checks and print only the numbers in between
+for num in range(num1, num2 + 1):
+    print(num)
