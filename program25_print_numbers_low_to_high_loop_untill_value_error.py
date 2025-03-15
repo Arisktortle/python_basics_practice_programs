80c4f8102ef45e9b0255e3263bb7c2d4c78511b6
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:52:04 +0800

    re-doing the program24, got reversed (PSEUDOCODE)

---
+++ program25_print_numbers_low_to_high_loop_untill_value_error.py
@@ -0,0 +1,6 @@
+#create a list for number inputs
+#input numbers and loop untill value error
+#check if valid input, then add to the list
+#if invalid input break the loop
+#arrange the list in ascending order
+#print the list from lowest to highest
