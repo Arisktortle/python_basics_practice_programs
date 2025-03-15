593f560d9fa0876fd0982f1e1054c1fb30617158
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 09:16:25 +0800

    create list for number input

--- program23_ask_user_for_unique_or_duplicate_loop_if_value_error.py
+++ program23_ask_user_for_unique_or_duplicate_loop_if_value_error.py
@@ -1,4 +1,6 @@
 #intialize a list for number inputs
+entered_numbers = []
+
 #create loop asking numbers for and checks if duplicate or unique
 #add the inputted number in the list
 #terminate the program if invalid input
