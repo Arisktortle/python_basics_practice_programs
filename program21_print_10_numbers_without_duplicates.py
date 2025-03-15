30b311697ac599d4c2e0f6f51e7fc44acacfe505
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 09:01:14 +0800

    check for duplicates and print numbers without dupes

--- program21_print_10_numbers_without_duplicates.py
+++ program21_print_10_numbers_without_duplicates.py
@@ -2,4 +2,6 @@
 nums = [int(input(f"Enter number {a+1}: ")) for a in range(10)]
 
 #loop that checks and display numbers that don't have duplicate
-#print the numbers
+for num in nums:
+    if nums.count(num) == 1:
+        print(num) #print the numbers
