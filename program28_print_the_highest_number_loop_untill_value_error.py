f384df2e3ff5c99e1ee4e2f28558d9a053a48319
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:30:35 +0800

    create loop that asks for numbers and check if valid, terminate if invalid input

--- program28_print_the_highest_number_loop_untill_value_error.py
+++ program28_print_the_highest_number_loop_untill_value_error.py
@@ -2,7 +2,19 @@
 highest_number = None
 
 #create loop that input numbers 
-#checks if the input is valid
+while True:
+    user_input = input("Please enter a number: ")
+
+    #checks if the input is valid
+    if user_input.isdigit() or (user_input.startswith('-') and user_input[1:].isdigit()):
+        number = int(user_input)
+        
 #intialize in the count and store in the variable if highest number
+
 #terminate the program if invalid input
+    else:
+
+        print("Invalid input! Exiting...")
+        break
+    
 #print the highest number input
