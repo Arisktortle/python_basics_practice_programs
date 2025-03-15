96a3bd941d02694a7a5dc809ec1d14080ae25fc9
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 09:26:21 +0800

    pseudocode

---
+++ program24_print_numbers_high_to_low_loop_untill_value_error.py
@@ -0,0 +1,6 @@
+#create a list for number input
+#loop that ask to input numbers
+#adds the inputted number into the list
+#check for value error, terminate if yes
+#sort the number inputted in decreasing
+#print the inputted numbers from the list
