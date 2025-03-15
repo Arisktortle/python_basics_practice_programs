f0da68f4c861bbbfedd3c500de6971129febbf1c
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:46:46 +0800

    loop that input numbers and check for valid, break loop if invalid input

--- program30_input_numbers_untill_value_error_and_compute_average.py
+++ program30_input_numbers_untill_value_error_and_compute_average.py
@@ -3,7 +3,17 @@ sum = 0
 numbers_count = 0
 
 #create loop that asks user for numbers untill value error
-#checks if the input is valid
-#if the input is valid break the loop
+while True:
+    user_input = input("Please enter a number: ")
+
+    if user_input.isdigit() or (user_input.startswith('-') and user_input[1:].isdigit()): #checks if the input is valid
+        number = int(user_input)
+        total_sum += number  
+        count += 1  #count the total number
+        
+    else:
+        print("Invalid input! Exiting...") #if the input is valid break the loop
+        break
+    
 #calculate the average by getting the sum of all divided by the total count
 #print the result
