3d4a23d8bb9702221754cc520e3120735feef7df
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:43:15 +0800

    pseudocode

---
+++ program30_input_numbers_untill_value_error_and_compute_average.py
@@ -0,0 +1,6 @@
+#create value for sum and total count of numbers
+#create loop that asks user for numbers untill value error
+#checks if the input is valid
+#if the input is valid break the loop
+#calculate the average by getting the sum of all divided by the total count
+#print the result
