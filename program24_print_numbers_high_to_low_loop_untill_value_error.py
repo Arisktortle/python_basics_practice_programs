9ee741133337f423a1e12a6116d37717dbce2bb9
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 09:29:58 +0800

    places the inputted numbers in the number list

--- program24_print_numbers_high_to_low_loop_untill_value_error.py
+++ program24_print_numbers_high_to_low_loop_untill_value_error.py
@@ -5,7 +5,9 @@ numbers = []
 while True:
     try:
         num = int(input("Enter a number (or type something non-numeric to exit): "))
+        
 #adds the inputted number into the list
+        numbers.append(num)
 
 #check for value error, terminate if yes
     except ValueError:
