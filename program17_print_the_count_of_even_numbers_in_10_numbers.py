b7fd685a959770547703bd017fee51727470f662
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 08:12:51 +0800

    checks if the number inputted is even and intialized a value for the count of even numbers

--- program17_print_the_count_of_even_numbers_in_10_numbers.py
+++ program17_print_the_count_of_even_numbers_in_10_numbers.py
@@ -1,6 +1,10 @@
 #input 10 numbers
+even = 0
 for a in range(10):
     num = int(input(f"Enter number {a+1}: "))
     
 #check and count the even numbers
+    if num % 2 == 0:
+        even += 1
+        
 #print the count
