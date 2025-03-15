d9668028dc1366cb17a5fe80a3523f280e78248a
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 09:18:53 +0800

    loop prompt to enter numbers, check if dupe or unique

--- program23_ask_user_for_unique_or_duplicate_loop_if_value_error.py
+++ program23_ask_user_for_unique_or_duplicate_loop_if_value_error.py
@@ -2,5 +2,16 @@
 entered_numbers = []
 
 #create loop asking numbers for and checks if duplicate or unique
+while True:
+    try:
+        # Ask user to input a number
+        num = int(input("Enter a number (or type something non-numeric to exit): "))
+        
+        # Check if the number is unique or a duplicate
+        if entered_numbers.count(num) == 0:
+            print("Unique")
+        else:
+            print("Duplicate")
+            
 #add the inputted number in the list
 #terminate the program if invalid input
