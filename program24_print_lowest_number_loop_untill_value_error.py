a6f1a0db8861d8f626bdc4906ecdaf4e0a9a8830
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 09:51:30 +0800

    added storage for lowest number value and fixed description

--- program24_print_lowest_number_loop_untill_value_error.py
+++ program24_print_lowest_number_loop_untill_value_error.py
@@ -1,4 +1,6 @@
-#create a list for inputted numbers
+#create a variable to store a lowest number input
+lowest_number = None
+
 #create loop that input numbers
 #check for invalid input
 #if invalid input break loop
