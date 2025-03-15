9b3135c3ec2c52f3ba760d3427f1955094d29382
Synch Coding <dhdbb@DESKTOP-855IK2Q>
2025-03-15 10:17:02 +0800

    pseudocode

---
+++ program27_print_the_number_with_most_duplicates.py
@@ -0,0 +1,6 @@
+#create a dictionary to store number inputs
+#input 10 numbers
+#count for the number times the number is inputted
+#if an input is invalid exit program
+#check the number with the most duplicates
+#print that number
